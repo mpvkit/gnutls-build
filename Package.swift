ddb3b35d4147d101a2f6c6009cc1729d31bd09de
cxfksword <718792+cxfksword@users.noreply.github.com>
2024-07-08 19:42:17 +0800

    update build

--- Package.swift
+++ Package.swift
@@ -13,22 +13,22 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "gmp",
-            url: "https://github.com/mpvkit/libsmbclient-build/releases/download/3.8.3/gmp.xcframework.zip",
+            url: "https://github.com/mpvkit/gnutls-build/releases/download/3.8.3/gmp.xcframework.zip",
             checksum: "126c63d62c9081e820b223ae36679f1bedee20efa88d63fc7ba5edac33aaabbc"
         ),
         .binaryTarget(
             name: "nettle",
-            url: "https://github.com/mpvkit/libsmbclient-build/releases/download/3.8.3/nettle.xcframework.zip",
+            url: "https://github.com/mpvkit/gnutls-build/releases/download/3.8.3/nettle.xcframework.zip",
             checksum: "288ef1fe3a01721cc326148ea15f0c16c282a42cadb3f0de6df5c16680105d57"
         ),
         .binaryTarget(
             name: "hogweed",
-            url: "https://github.com/mpvkit/libsmbclient-build/releases/download/3.8.3/hogweed.xcframework.zip",
+            url: "https://github.com/mpvkit/gnutls-build/releases/download/3.8.3/hogweed.xcframework.zip",
             checksum: "6885465672a1e697ea3919fc65c7c9115016a0b546fbc5a6019ee85272f20c3d"
         ),
         .binaryTarget(
             name: "gnutls",
-            url: "https://github.com/mpvkit/libsmbclient-build/releases/download/3.8.3/gnutls.xcframework.zip",
+            url: "https://github.com/mpvkit/gnutls-build/releases/download/3.8.3/gnutls.xcframework.zip",
             checksum: "bc45270ace9a9bcdf9d1e00f8d442880ac67706b81b7ef2b0d48ab1077de0964"
         )
     ]
